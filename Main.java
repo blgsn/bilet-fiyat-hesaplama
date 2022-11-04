dde5b0bb7336e70df26db587293665bc1c520a1d
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-04 11:39:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,22 +6,25 @@ public class Main {
 		int age,typeTravel=0;
 		double KM,Price,travelSale=0.0,ageSale=0.0 ;
 		double totalPrice=0;
-		boolean isError=false;
+		//gets the values from the user
 		Scanner input=new Scanner(System.in);
 		System.out.print("Mesafeyi Km cinsinden giriniz:");
 		KM=input.nextDouble();
 		
 		System.out.print("Yaşınızı giriniz:");
 		age=input.nextInt();
-		
+		//gets the type of travel from the user
 		System.out.print("Yolculuk tipini giriniz(1 => Tek Yön , 2 => Gidiş Dönüş:");
 		typeTravel=input.nextInt();
-		
+		//calculates price
 		Price = KM * 0.10;
+		//entered age and km are negative
 		if(age<0 || KM<0)  {
 			System.out.println("Hatalı veri girdiniz!");
 		}
-		
+		//entered values are positive 
+		//when the user entered number different from 1 or 2 for types of travel,
+		//it gives the error message
 		else {
 			
 		switch(typeTravel) {
@@ -67,6 +70,7 @@ public class Main {
 			default:
 				 System.out.print("Hatalı veri girdiniz!");
 				 }	
+		//when the user entered the valid value 
 		if(typeTravel==1 || typeTravel==2 )
 		System.out.println("Toplam tutar:" + totalPrice);
 	}
